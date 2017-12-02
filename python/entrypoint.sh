645e73640ff75d8460e758dcb2d33f37dfc1308d
Alexey Safronov <alexey.safronov@synesis.ru>
2017-12-02 07:25:06 +0300

    python: delete environment vars from entrypoints

--- python/entrypoint.sh
+++ python/entrypoint.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-export DB_HOST=db
-export DB_NAME=mail_drive
-export DB_USER=leshka
-export DB_PASSWORD=leshka
-
-export MINIO_HOST=minio1
-export MINIO_PORT=9000
-export MINIO_ACCESS_KEY=set-value-here
-export MINIO_SECRET_KEY=set-value-here
-
 python3.5 wait_postgres.py pg_database
 python3.5 server.py
